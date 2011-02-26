2f17b525ab7157decb76f06d64ce79fb5d9a83e6
Heidi Meiniche <heidi@meiniche.net>
2011-02-26 21:00:40 +0100

    basis projekt

---
+++ src/ArkSpot/Classes/ArkSpotAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  ArkSpotAppDelegate.h
+//  ArkSpot
+//
+//  Created by Heidi Meiniche on 2/26/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface ArkSpotAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
